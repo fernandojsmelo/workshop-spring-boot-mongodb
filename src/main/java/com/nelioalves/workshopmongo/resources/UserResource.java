26342a186832e6ccfe9d905b66b5beb622e1db26
fernandojsmelo <fernandojsmelo@gmail.com>
2023-11-02 17:06:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -33,7 +33,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	@GetMapping("/{id}")
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
  	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
